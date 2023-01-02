7e099473de611901a4744a80e91cc2ceb6036107
maheshwaranv <mahes1287@gmail.com>
2023-01-02 09:41:35 +0100

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navigate, Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/new" element={<h1>New</h1>} />
-        <Route path="/:id">
-          <Route index element={<h1>Show</h1>} />
-          <Route path="edit" element={<h1>Edit</h1>} />
-        </Route>
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </div>
+    <Container className="my-4">
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/new" element={<h1>New</h1>} />
+          <Route path="/:id">
+            <Route index element={<h1>Show</h1>} />
+            <Route path="edit" element={<h1>Edit</h1>} />
+          </Route>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+    </Container>
   );
 }
 
